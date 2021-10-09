bc8fde4ed9a550c6f143c5f9fdb5f4011b1c0aa2
Gallaoui Achref <gallaoui101@gmail.com>
2021-10-09 13:08:39 +0100

    Routing/Product Template

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,8 +11,16 @@ class HomeController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('home/index.html.twig');
+    }
+    #[Route('/p', name: 'pannel')]
+    public function PannelIndex(): Response
+    {
+        return $this->render('panel/index.html.twig');
+    }
+    #[Route('/c', name: 'checkout')]
+    public function CheckIndex(): Response
+    {
+        return $this->render('checkout/index.html.twig');
     }
 }
---
+++ src/Entity/Product.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ProductRepository::class)
+ */
+class Product
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Description;
+
+    /**
+     * @ORM\Column(type="bigint")
+     */
+    private $Price;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $Available;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->Name;
+    }
+
+    public function setName(string $Name): self
+    {
+        $this->Name = $Name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->Description;
+    }
+
+    public function setDescription(string $Description): self
+    {
+        $this->Description = $Description;
+
+        return $this;
+    }
+
+    public function getPrice(): ?string
+    {
+        return $this->Price;
+    }
+
+    public function setPrice(string $Price): self
+    {
+        $this->Price = $Price;
+
+        return $this;
+    }
+
+    public function getAvailable(): ?bool
+    {
+        return $this->Available;
+    }
+
+    public function setAvailable(?bool $Available): self
+    {
+        $this->Available = $Available;
+
+        return $this;
+    }
+}
